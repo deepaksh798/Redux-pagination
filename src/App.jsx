4b91868dec24a18454307a3615878379de415f9e
deepaksh798 <deepak.sh798@gmail.com>
2024-07-31 14:05:57 +0530

    loading statement and card update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from './features/dataSlice';
@@ -11,23 +9,32 @@ function App() {
 
   const state = useSelector((state) => state.cardData)
 
-  if(state.isLoading === true) {
-    <h1>Loading...</h1>
-    }
-
+  useEffect(() => {
+      setTimeout(() => {
+      console.log("here");
+      const fatchdataFunc = () => {
+        dispatch(fetchData())
+      }
+      fatchdataFunc();
+    }, 5000);
+    }, [])
+    
   return (
     <>
-      <div>
-        <button onClick={() => dispatch(fetchData())}>Fetch data</button>
-        {state.data && state.data.map((data,index) => (
-          <div key={index}>
-            <h1>{data.title}</h1>
-            <p>{data.body}</p>
-        </div>
-        
-        ))}
-        
-      </div>
+        {/* <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={() => dispatch(fetchData())}>Fetch data</button> */}
+        <div >
+        {state.data ? <div className="grid md:grid-cols-3 gap-4">
+          { state.data.map((data,index) => (
+          <a key={index} href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div >
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data.title}</h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">{data.body}</p>
+            </div>
+          </a>   
+          ))}
+          </div>
+          : <h1 className='text-5xl'>Loading...</h1> }
+      </div>     
     </>
   )
 }
