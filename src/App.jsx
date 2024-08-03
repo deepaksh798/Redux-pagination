23b04c77b23aa35c7ed9ff1fd96d050b5d8e9458
deepaksh798 <deepak.sh798@gmail.com>
2024-08-03 12:47:13 +0530

    adding pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,98 @@
-import { useEffect } from 'react'
-import './App.css'
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from './features/dataSlice';
 
 function App() {
-  
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
 
-  const state = useSelector((state) => state.cardData)
+  const state = useSelector((state) => state.cardData);
+  console.log('state', state.data);
+
+  const fetchDataFunc = () => {
+    dispatch(fetchData());
+  };
 
   useEffect(() => {
-      setTimeout(() => {
-      console.log("here");
-      const fatchdataFunc = () => {
-        dispatch(fetchData())
-      }
-      fatchdataFunc();
-    }, 5000);
-    }, [])
-    
+    setTimeout(() => {
+      console.log('here');
+      fetchDataFunc();
+    }, 3000);
+  }, []);
+
+  const selectPageHandler = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= Math.ceil(state.data.length / 10) &&
+      selectedPage !== page
+    )
+      setPage(selectedPage);
+  };
+
   return (
     <>
-        {/* <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={() => dispatch(fetchData())}>Fetch data</button> */}
-        <div >
-        {state.data ? <div className="grid md:grid-cols-3 gap-4">
-          { state.data.map((data,index) => (
-          <a key={index} href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <div >
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data.title}</h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">{data.body}</p>
+      <div>
+        {state.data ? (
+          <div>
+            <h1 className="mb-6 text-3xl font-semibold bg-slate-400">React Pagination</h1>
+            <div className="grid md:grid-cols-3 gap-4">
+              {state.data.slice(page * 10 - 10, page * 10).map((data, index) => (
+                <a
+                  key={index}
+                  href="#"
+                  className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+                >
+                  <div>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {data.title}
+                    </h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">{data.body}</p>
+                  </div>
+                </a>
+              ))}
             </div>
-          </a>   
-          ))}
+            {state.data.length > 0 && (
+              <div className="pt-8">
+                <ul className="inline-flex -space-x-px text-base h-10">
+                  <li>
+                    <a
+                      className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                      onClick={() => selectPageHandler(page - 1)}
+                    >
+                      Previous
+                    </a>
+                  </li>
+
+                  {[...Array(Math.ceil(state.data.length / 10))].map((_, i) => (
+                    <a
+                      className={
+                        page === i + 1
+                          ? 'flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+                          : 'flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'
+                      }
+                      onClick={() => selectPageHandler(i + 1)}
+                      key={i}
+                    >
+                      {i + 1}
+                    </a>
+                  ))}
+                  <a
+                    className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                    onClick={() => selectPageHandler(page + 1)}
+                  >
+                    Next
+                  </a>
+                </ul>
+              </div>
+            )}
           </div>
-          : <h1 className='text-5xl'>Loading...</h1> }
-      </div>     
+        ) : (
+          <h1 className="text-5xl">Loading...</h1>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
